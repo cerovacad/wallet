9b0f9f146a4f4f6c98237fae351a671a61b5dc8a
MajaMilanovic <maja_milanovic@outlook.com>
2018-08-07 13:26:55 +0200

    add lucid wallet style flex

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -5,17 +5,19 @@ import styles from "../styles/styles.scss";
 const Wallet = () => {
     return (
         // <div className={styles.container}>
-            <div className={styles.wallet}>
-                <h5 className={styles.wallet__header}>Lucid Wallet</h5>
-                <div className={styles.wallet__card}>
-                    <div className={styles.wallet__card__content}>
-                        <p className={styles.wallet__card__content__balance}>1,748</p>
-                        <p className={styles.wallet__card__content__currency}>BTC</p>
+        <div className={styles.wallet}>
+            <h5 className={styles.wallet__header}>Lucid Wallet</h5>
+            <div className={styles.wallet__card}>
+                <div className={styles.wallet__card__content}>
+                    <p className={styles.wallet__card__content__balance}>1,748</p>
+                    <p className={styles.wallet__card__content__currency}>BTC</p>
+                    <div className={styles.wallet__card__content__buttonGroup}>
                         <button className={styles.wallet__card__content__button__blue} >Top-up account</button>
                         <button className={styles.wallet__card__content__button__grey}>Transfer to Bank</button>
                     </div>
                 </div>
             </div>
+        </div>
         // </div>
     );
 };
