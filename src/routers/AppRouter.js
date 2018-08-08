fce460ac3ecc50c59e1a61507e366b7d7f6174a7
MajaMilanovic <mayam893@gmail.com>
2018-08-08 03:14:36 -0700

    add signup

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-import Wallet from "../components/Wallet";
+import React from 'react'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import Wallet from '../components/Wallet'
+import Signup from '../pages/Signup'
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/wallet" component={Wallet} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
-};
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/wallet' component={Wallet} />
+          <Route path='/signup' component={Signup} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
 
-export default AppRouter;
+export default AppRouter
