1ac401ac8e51c1f1092abc5a4206cd1d273e798b
grey <greyknight@live.com>
2018-08-14 11:38:55 +0200

    Added transactionActions

--- src/actions/transactionActions.js
+++ src/actions/transactionActions.js
@@ -1,5 +1,42 @@
-export const REQUESING = 'REQUESTING',
-             IS_REQUESING = 'IS_REQUESTING',
-             SENDING = 'SENDING',
-             IS_SENDING = 'IS_SENDING';
+import uid from 'uid'
+export const ADD_TRANSACTION = 'ADD_TRANSACTION',
+    REMOVE_TRANSACTION= 'REMOVE_TRANSACTION',
+
+
+// INITIAL STATE
+// type: 'incoming',
+// title: 'Website changes',
+// description: 'requesting',
+// user: 'example@gmail.com',
+// status: 'in-progress',
+// balance: 0.008,
+// longDescription: 'Simply dummy text of the printing anddummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make'
+
+export const addTransaction = (  {
+    type = '',
+    title = '',
+    description = '',
+    user = '',
+    status= '',
+    balance = 0,
+    longDescription = ''
+  } = {}) => ({
+    type: ADD_TRANSACTION,
+    transaction: {
+        id:uid(),
+        type,
+        title,
+        description,
+        user,
+        status,
+        balance,
+        longDescription
+    }
+    
+});
+
+export const removeTransaction = ({id} = {}) => ({
+    type: REMOVE_TRANSACTION,
+    id
+});
 
